b291787dbc8b2bc5d8f0b8e469422891a5894562
kjgamis <kjgamis@live.ca>
2019-05-09 23:38:44 -0400

    finish module 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,6 @@
-const getNotes = require('./notes.js')
+const chalk = require('chalk')
 
+const getNotes = require('./notes.js')
 console.log(getNotes('Hello'))
+
+console.log(chalk.green.bold.inverse('SUCCESS!'))
