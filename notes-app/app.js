2ec858be016ec2e26fbc58f051c611423e4499c2
kjgamis <kjgamis@live.ca>
2019-05-11 15:36:30 -0400

    adding a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
@@ -23,7 +23,7 @@ yargs.command({
         }
     },
     handler: (argv) => {
-        console.log(`Title: ${argv.title} - ${argv.body}`)
+        notes.addNote(argv.title, argv.body)
     } 
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,50 @@
-function getNotes(notes) {
+const fs = require('fs')
+
+const getNotes = (notes) => {
   return 'Your notes... ' + notes
 }
 
-module.exports = getNotes
+const addNote = (title, body) => {
+  // json file is added to 'notes' array
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter((note) => {
+    // if title already exists, duplicatedNotes will store the duplicate
+    return note.title === title
+  })
+
+  // if there are no duplicated, save the note as normal
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body
+    })
+    saveNotes(notes)
+    console.log('New note added!')
+  } else {
+    console.log('Note title taken!')
+  }
+}
+
+
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+// loadNotes reads the json file
+const loadNotes = () => {
+  // error handling: read JSON file if it exists, if not then return empty array which is the value of the 'notes' array above. The array is stringified and added to notes.json
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch(e) {
+    return []
+  }
+}
+
+module.exports = {
+  getNotes,
+  addNote
+}
