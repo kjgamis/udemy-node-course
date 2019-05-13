425606897b4d42edd1077bcb2d33b75481ae9f86
kjgamis <kjgamis@live.ca>
2019-05-12 22:20:21 -0400

    refactor and remove getNotes function

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,10 +1,6 @@
 const chalk = require('chalk')
 const fs = require('fs') 
 
-const getNotes = (notes) => {
-  return 'Your notes... ' + notes
-}
-
 const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes)
   fs.writeFileSync('notes.json', dataJSON)
@@ -26,14 +22,16 @@ const loadNotes = () => {
 const addNote = (title, body) => {
   // json file is added to 'notes' array
   const notes = loadNotes()
-  const duplicateNotes = notes.filter((note) => {
-    // if title already exists, duplicatedNotes will store the duplicate
-    return note.title === title
-  })
+  // const duplicateNotes = notes.filter((note) => {
+  //   // if title already exists, duplicatedNotes will store the duplicate
+  //   return note.title === title
+  // })
+  // .find() will stop the rest of the program from running when it finds a duplicate
+  const duplicateNote = notes.find((note) => note.title === title)
 
   // if there are no duplicated, save the note as normal
   let message
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title,
       body
@@ -66,16 +64,28 @@ const removeNote = (title) => {
 
 // List Notes
 const listNotes = () => {
-  console.log(chalk.green.inverse('Your notes:'))
+  console.log(chalk.white.inverse('Your notes:'))
   const notes = loadNotes()
   notes.forEach(note => {
-    console.log(chalk.green(note.title))
+    console.log(chalk.white(note.title))
   })
 }
 
+const readNote = (title) => {
+  const notes = loadNotes()
+  const note =  notes.find(note => note.title === title)
+
+  if (note) {
+    console.log(chalk.green.inverse(note.title))
+    console.log(note.body)
+  } else {
+    console.log(chalk.red.inverse('Note not found!'))
+  }
+}
+
 module.exports = {
-  getNotes,
   addNote,
   removeNote,
-  listNotes
+  listNotes,
+  readNote
 }
