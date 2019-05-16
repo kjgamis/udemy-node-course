13735a84607f489e46fcd5cf905ac59a9a93ce3c
kjgamis <kjgamis@live.ca>
2019-05-16 00:04:46 -0400

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,9 +9,14 @@ request({
     // const data = JSON.parse(response.body)
     // console.log(data.currently)
     // console.log(response.body.currently)
-
-    const { temperature, precipProbability, precipType } = response.body.currently
-    // console.log(`${response.body.daily.data[0].summary} It is currently ${temperature} degrees out. There is a ${precipProbability*100}% of ${precipType}.`)
+    if (error) {
+        console.log('Unable to connect to weather service.')
+    } else if (response.body.error) {
+        console.log('Unable to find location.')
+    } else {
+        const { temperature, precipProbability, precipType } = response.body.currently
+        console.log(`${response.body.daily.data[0].summary} It is currently ${temperature} degrees out. There is a ${precipProbability*100}% of ${precipType}.`)
+    }
 })
 
 const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia2pnYW1pcyIsImEiOiJjanZxMnhsOHMwMzU5M3lyeDc1d3NoeW9rIn0.oP2_OQ10lm1DRyS0K_oPxg&limit=1'
@@ -19,7 +24,13 @@ const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.js
 request({
     url: url2, json: true
 }, (error, response) => {
-    const lat = response.body.features[0].center[1]
-    const long = response.body.features[0].center[0]
-    console.log(lat, long)
+    if (error) {
+        console.log('Unable to connect to map service')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const lat = response.body.features[0].center[1]
+        const long = response.body.features[0].center[0]
+        console.log(lat, long)
+    }
 })
