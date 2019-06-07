6b3416e89de3fa66dda659a212a46d8f1ab26b23
kjgamis <kjgamis@live.ca>
2019-06-07 15:14:48 -0400

    browser HTTP requests with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,21 @@
 console.log('Client side JavaScript code loaded.')
+
+const url = 'http://puzzle.mead.io/puzzle'
+
+fetch(url).then(response => {
+    response.json().then(data => {
+      console.log(data)
+    })
+  })
+
+fetch('/weather?address=toronto').then(response => {
+  response.json().then(data => {
+    if (data.error) {
+       console.log(data.error)
+    } else {
+      console.log(data.address)
+      console.log(data.forecast)
+      console.log(data.location)
+    }
+  })
+})
