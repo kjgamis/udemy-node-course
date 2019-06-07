c69eca8f1ba427b19f72be711e5b2099207a20ef
kjgamis <kjgamis@live.ca>
2019-06-07 10:48:56 -0400

    query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -50,9 +50,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
     res.send({
+        forecast: 'sunny',
         location: 'Toronto',
-        forecast: 'sunny'
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term!'
+        })
+    }
+    console.log(req.query)
+    res.send({
+        products: []
     })
 })
 
