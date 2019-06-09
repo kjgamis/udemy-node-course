1a70b6081a7197d45975beb264a8dab37821e97f
kjgamis <kjgamis@live.ca>
2019-06-09 16:49:46 -0400

    inserting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,10 +14,51 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => 
     console.log('Connected to MongoDB successfully')
     const db = client.db(dbName)
 
-    db.collection('users').insertOne({
-        name: 'Karen Gamis',
-        age: 22
+    // db.collection('users').insertOne({
+    //     name: 'Karen Gamis',
+    //     age: 22
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user.')
+    //     }
+    //     // output from the insertOne operation
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jane Doe',
+    //         age: 23
+    //     }, {
+    //         name: 'John Doe',
+    //         age: 24
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert users.')
+    //     }
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'buy supplies',
+            completed: false
+        },
+        {
+            description: 'eat ice cream',
+            completed: true
+        },
+        {
+            description: 'watch movie',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks.')
+        }
+        console.log(result.ops)
     })
 
-    // client.close()
+    client.close()
 })
