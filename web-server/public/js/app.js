8ce4dd68798d95fe9cd434b2b37d91b24e4989c2
kjgamis <kjgamis@live.ca>
2019-09-22 17:58:57 -0400

    change localhost address for local env, make fetch api url agnostic, remove console.log

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -11,18 +11,14 @@ weatherForm.addEventListener('submit',(e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  fetch('http://localhost:3000/weather?address=' + location).then(response => {
+  fetch('/weather?address=' + location).then(response => {
     response.json().then(data => {
       if (data.error) {
-        console.log(data.error)
         messageOne.textContent = data.error
         messageTwo.textContent = ''
       } else {
         messageOne.textContent = data.location
         messageTwo.textContent = data.forecast
-        console.log(data.address)
-        console.log(data.forecast)
-        console.log(data.location)
       }
     })
   })
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,7 +10,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 // standard to use env variables
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 // paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -112,5 +112,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port 3000')
+    console.log(`Server is up on port ${port}`)
 })
